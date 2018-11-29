bdfbb379af5257549b8ac72e8eecae1bedea6bb5
wumengya <wumengya@aliyun.com>
2018-11-29 17:47:59 +0800

    第一次提交

---
+++ src/components/myLesson.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="myLesson bgmain mianScroll">
+        <top></top>
+        <searchTop></searchTop>
+        <div class="suspendTool">
+            <router-link to="/newCourse"><i class="el-icon-plus"></i></router-link>
+            <a><i class="el-icon-more"></i></a>
+        </div>
+
+        <div class="lessonList">
+            <ul>
+                <li v-for="(course,index) in courseList" :key="index">
+                        <em v-if="course.shareState == true" class="shareState have">
+                            已校共享
+                        </em>
+                        <div class="lessonImg"><img v-bind:src="course.imgUrl"></div>
+                        <div class="lessonContent">
+                            <h4>{{course.title}}<i class="el-icon-star-off"></i></h4>
+                            <p class="synopsis"><span><i class="icon bpMobile bpMobile-wode2"></i>{{course.formPeo}}</span><span><i class="icon bpMobile bpMobile-hs_h_Clock_h-naozhong"></i>{{course.time}}</span>   
+                            </p>
+                            <div class="operate">
+                                <a v-if="course.haveReflect == false" @click="tcshow1 =! tcshow1"><i class="el-icon-edit"></i>添加反思</a>
+                                <a v-else @click="tcshow2 =! tcshow2"><i class="el-icon-view"></i>查看反思</a>
+                                <a @click="tcshow =! tcshow"><i class="el-icon-location-outline"></i>课程定位</a>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                </li>
+             </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import top from '@/components/top'
+import searchTop from '@/components/searchTop'
+// import axios  from 'axios'
+export default {
+    name:'myLesson',
+    components:{top,searchTop},
+    data(){
+        return{
+            tab:{
+                popupMoreTrigger: false
+            },
+            courseList:[
+                        {
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅1',
+                            formPeo:'王艳丽',
+                            time:'2018-10-22',
+                            shareState:true,
+                            haveReflect:true
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅2',
+                            formPeo:'张洋',
+                            time:'2018-10-22',
+                            shareState:false,
+                            haveReflect:false
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅3',
+                            formPeo:'王帅',
+                            time:'2018-10-22',
+                            shareState:false,
+                            haveReflect:true
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅3',
+                            formPeo:'王帅',
+                            time:'2018-10-22',
+                            shareState:false,
+                            haveReflect:true
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅3',
+                            formPeo:'王帅',
+                            time:'2018-10-22',
+                            shareState:false,
+                            haveReflect:true
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅3',
+                            formPeo:'王帅',
+                            time:'2018-10-22',
+                            shareState:false,
+                            haveReflect:true
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅3',
+                            formPeo:'王帅',
+                            time:'2018-10-22',
+                            shareState:false,
+                            haveReflect:true
+                        },{
+                            imgUrl:require('./../assets/images/runtu.jpg'),
+                            title:'少年闰土——鲁迅4',
+                            formPeo:'李炜',
+                            time:'2018-10-22',
+                            shareState:true,
+                            haveReflect:false
+                        }
+                    ],
+                t: 2,
+                page:1,
+                rows:10,
+                schoolStateList:[],
+                schoolStateDetail:{}
+        }
+    },
+    mounted(){
+        this.loadList();
+        // this.loadList1();
+    },
+    methods:{
+        TogglePopupMore(){
+            this.tab.popupMoreTrigger = !this.tab.popupMoreTrigger;
+        },
+        loadList:function(){
+           var url = "/cao/api/Plan/GetTermList";
+           var _this = this;
+           _this.$ajax.post(url,{
+               params:{termId:1}
+           }).then(res=>{
+                console.log(res.data);
+           }).catch(error=>{
+                console.log(error);
+           })
+        },
+        loadList1:function(){
+           this.$ajax.get('/cao/api/Plan/GetTermList').then(res=>{
+               console.log(res);
+           }).catch(error=>{
+               console.log(error);
+           })
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/screenPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="screenPage">
+        <mt-picker :slots="slots" @change="onValuesChange"></mt-picker>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'screenPage',
+    data(){
+        return{
+             slots: [
+                {
+                flex: 1,
+                values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+                className: 'slot1',
+                textAlign: 'center'
+                }, {
+                divider: true,
+                content: '-',
+                className: 'slot2'
+                }, {
+                flex: 1,
+                values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+                className: 'slot3',
+                textAlign: 'center'
+                }, {
+                divider: true,
+                content: '-',
+                className: 'slot4'
+                }, {
+                flex: 1,
+                values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+                className: 'slot5',
+                textAlign: 'center'
+                }
+            ]
+        }
+    },
+    methods:{
+        onValuesChange(picker, values) {
+        if (values[0] > values[1]) {
+            picker.setSlotValue(1, values[0]);
+        }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
